96978e6e9cdf0824b90ef7ade942687bca312b07
vadim-tkachenko <vadym.tkachenko@privatbank.ua>
2021-10-30 22:44:32 +0300

    init commit

--- src/com/pb/tkachenko/hm4/Anagram.java
+++ src/com/pb/tkachenko/hm4/Anagram.java
@@ -1,28 +1,31 @@
 package com.pb.tkachenko.hm4;
 
+import java.util.Arrays;
+
 public class Anagram {
     public static void main(String[] args) {
-        System.out.println(isAnagram("Аз есмь строка, живу я, мерой остр. -> За семь морей ростка я вижу рост"));
-        System.out.println(isAnagram("Я в мире — сирота. -> Я в Риме — Ариост"));
-        System.out.println(isAnagram("Я хочу жить в мире -> Ром жизнь круто"));
+        System.out.println(isAnagram("Аз есмь строка живу я мерой остр", "За семь морей ростка я вижу рост"));
+        System.out.println(isAnagram("Я в мире — сирота", "Я в Риме — Ариост"));
+        System.out.println(isAnagram("Я хочу жить в мире", "Ром жизнь круто"));
     }
-    public static boolean isAnagram (String text){
-        /*
-        isAnagram(String)->boolean;
 
-        >>isAnagram("Аз есмь строка, живу я, мерой остр. -> За семь морей ростка я вижу рост") -> true
-        >>isAnagram("Я в мире — сирота. -> Я в Риме — Ариост") -> true
-        >>isAnagram("Я хочу жить в мире -> Ром жизнь круто") -> false
-         */
-        String s1 = text.toUpperCase().replaceAll("[^А-Яа-я]", "");
-        StringBuffer s2 = new StringBuffer(s1).reverse();
-        boolean answer = true;
-        for (int count=0; count<s1.length(); count++){
-            if (s1.charAt(count) !=s2.charAt(count)){
-                answer = false;
-                break;
-            }
+    static boolean isAnagram(String str1, String str2) {
+        String s1 = str1.replaceAll("\\s", "");
+        String s2 = str2.replaceAll("\\s", "");
+        boolean status = true;
+        if (s1.length() != s2.length()) {
+            status = false;
+        } else {
+            char[] ArrayS1 = s1.toLowerCase().toCharArray();
+            char[] ArrayS2 = s2.toLowerCase().toCharArray();
+            Arrays.sort(ArrayS1);
+            Arrays.sort(ArrayS2);
+            status = Arrays.equals(ArrayS1, ArrayS2);
         }
-        return answer;
+        if (status) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- src/com/pb/tkachenko/hm4/CapitalLetter.java
+++ src/com/pb/tkachenko/hm4/CapitalLetter.java
@@ -1,17 +1,24 @@
 package com.pb.tkachenko.hm4;
 
+import java.util.Locale;
+
 public class CapitalLetter {
     public static void main(String[] args) {
 
-        String name = "Истина — это то, что выдерживает проверку опытом. Эйнштейн А.";
-        System.out.println("Original String: " + name);
-        String firstLetStr = name.substring(0, 1);
-        String remLetStr = name.substring(1);
+        String name = "истина — это то, что выдерживает проверку опытом. Эйнштейн А.";
+        for (int i = 0; i < name.length(); i++) {
+            if (isLetterOrDigit(name.charAt(i)) && (i == 0 || !isLetterOrDigit(name.charAt(i - 1)))){
+                char[] nameChars = name.toCharArray();
+                nameChars[i] = Character.toUpperCase(nameChars[i]);
+                name = String.valueOf(nameChars);
+            }
+        }
+        System.out.println(name);
+    }
 
-        firstLetStr = firstLetStr.toUpperCase();
-
-        String firstLetterCapitalizedName = firstLetStr + remLetStr;
-        System.out.println("String with first letter as Capital: " + firstLetterCapitalizedName);
-
+    private static boolean isLetterOrDigit(char c) {
+        return (c >= 'а' && c <= 'я') ||
+                (c >= 'А' && c <= 'Я') ||
+                (c >= '0' && c <= '9');
     }
 }
